8939c98ad6544ce48ad79282b7d0cb9026cf26d5
Oksana Harris <oksanaharris@Oksanas-MacBook-Pro.local>
2017-07-22 19:49:33 -1000

    finished

--- indexing.sql
+++ indexing.sql
@@ -12,6 +12,7 @@ SELECT make_title FROM car_models WHERE make_code = 'LAM' GROUP BY make_title;
 -- Time: 1.326 ms - with all three: make_code, model_title, make_title
 -- Time: 1.171 ms - with all four: make_code, model_code, model_title, make_title
 -- Time: 1.021 ms - with all four and year
+-- Time: 1.061 ms - when indexing at table creation
 
 SELECT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = 'GT-R' GROUP BY model_title;
 -- time: 31.340 ms
@@ -23,6 +24,7 @@ SELECT model_title FROM car_models WHERE make_code = 'NISSAN' AND model_code = '
 -- Time: 1.983 ms - with all three: make_code, model_title, make_title
 -- Time: 1.154 ms - with all four: make_code, model_code, model_title, make_title
 -- Time: 1.571 ms - with all four and year - with all four and year
+-- Time: 1.661 ms - when indexing at table creation
 
 SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code = 'LAM';
 -- Time: 32.835 ms
@@ -34,6 +36,7 @@ SELECT make_code, model_code, model_title, year FROM car_models WHERE make_code 
 -- Time: 1.740 ms - with all three: make_code, model_title, make_title
 -- Time: 1.544 ms - with all four: make_code, model_code, model_title, make_title
 -- Time: 1.513 ms - with all four and year - with all four and year
+-- Time: 1.531 ms - when indexing at table creation
 
 SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
 -- Time: 79.540 ms
@@ -44,6 +47,7 @@ SELECT * FROM car_models WHERE year BETWEEN 2010 AND 2015;
 -- Time: 73.995 ms - with make_code_index AND make_title_index
 -- Time: 67.092 ms - with all three: make_code, model_title, make_title
 -- Time: 62.283 ms - with all four and year
+-- Time: 59.961 ms - when indexing at table creation
 
 SELECT * FROM car_models WHERE year = 2010;
 -- Time: 44.501 ms
@@ -54,6 +58,7 @@ SELECT * FROM car_models WHERE year = 2010;
 -- Time: 44.282 ms - with make_code_index AND make_title_index
 -- Time: 45.312 ms - with all three: make_code, model_title, make_title
 -- Time: 14.145 ms - with all four and year
+-- Time: 14.006 ms - when indexing at table creation
 
 CREATE INDEX make_code_index
   ON car_models (make_code);
